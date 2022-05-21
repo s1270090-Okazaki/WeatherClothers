790d01542363c39e423ca83cf5cc58976a3f7083
s1270090-Okazaki <49747862+s1270090-Okazaki@users.noreply.github.com>
2022-05-21 15:01:49 +0900

    Initial Commit

---
+++ WeatherClothes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherClothes
+//
+//  Created by 岡崎慶汰 on 2022/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
